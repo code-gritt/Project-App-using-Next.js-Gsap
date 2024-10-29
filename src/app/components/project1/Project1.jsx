b3c07ac6342c9bf7e84bbe3bcfe3f4b861eebc68
gokul <gokulchandan24@gmail.com>
2024-10-29 20:46:40 +0530

    changes made

--- src/app/components/project1/Project1.jsx
+++ src/app/components/project1/Project1.jsx
@@ -8,7 +8,6 @@ const Project1 = () => {
       <div className="greenproject1"></div>
       <div className="content">
         <div className="title">
-          {/* <div className="small"></div> */}
           <div className="big md:text-[50px] lg:text-[100px] text-[50px] md:top-0 top-[50px]">
             <span>J</span>
             <span>o</span>
@@ -24,22 +23,26 @@ const Project1 = () => {
             <span>p</span>
           </div>
         </div>
-        <div className="project1subtitle1 p-2 gap-4 text-[#000000] justify-center text-xl">
-          <span className="">
-            <b className="font-bold">Description: {""}</b>
+
+        <div className="project1subtitle1 block max-w-3xl p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Description
+          </h5>
+          <p className="font-normal text-gray-700 dark:text-gray-400">
             Built a full stack job portal application from scratch using Next JS
             14, the user can login as a candidate or as a recruiter, post a job,
             edit, update, delete a job. Lots of states have been managed inside
             next.js server components.
-          </span>
-        </div>
-        <div className="project1subtitle p-2 gap-4 text-[#000000] justify-center text-xl">
-          <span className="">
-            <b className="font-bold">Technologies used: {""}</b>
+          </p>
+
+          <h5 className="mb-2 mt-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Technologies used
+          </h5>
+          <p className="font-normal text-gray-700 dark:text-gray-400">
             React.js · Next.js · Tailwind CSS · Mongoose Web Server · MongoDB ·
             HTML5 · Cascading Style Sheets (CSS) · Front-End Development ·
             Back-End Web Development · Node.js
-          </span>
+          </p>
         </div>
       </div>
     </>
