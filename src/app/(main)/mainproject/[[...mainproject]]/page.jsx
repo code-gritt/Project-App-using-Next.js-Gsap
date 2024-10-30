daa497aa168dcd42bd946191bc1a28d9dfed0d6e
gokul <gokulchandan24@gmail.com>
2024-10-30 19:16:13 +0530

    intro page created

---
+++ src/app/(main)/mainproject/[[...mainproject]]/page.jsx
@@ -0,0 +1,253 @@
+"use client";
+import React, { useEffect, useRef, useState, useLayoutEffect } from "react";
+import Head from "next/head";
+import { Swiper, SwiperSlide } from "swiper/react";
+<Head>
+  <link rel="stylesheet" href="https://unpkg.com/swiper/css/swiper.min.css" />
+  <link rel="stylesheet" href="https://unpkg.com/swiper/css/pagination.css" />
+</Head>;
+import { useSwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import img1 from "@/../public/img-1.png";
+import img2 from "@/../public/img-2.png";
+import img3 from "@/../public/img-3.png";
+import { Pagination } from "swiper/modules";
+import { gsap } from "gsap";
+import Image from "next/image";
+import Project1 from "../../../components/project1/Project1";
+
+const Main = () => {
+  const [active, setActive] = useState();
+
+  const swiperSlide = useSwiperSlide();
+  const root = useRef();
+  useEffect(() => {
+    console.log(active, "active");
+  }, [active]);
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      if (active === 0 || active === undefined) {
+        gsap.from(".first .yellowproject1", { y: "-1000", duration: 2 });
+        gsap.from(".first .blackproject1", { y: "1000", duration: 2 });
+        gsap.from(".first .greenproject1", { y: "500", duration: 3 });
+        gsap.from(".first img", { x: "1000", duration: 2 });
+        gsap.from(".first .small", { y: "100", opacity: 0, delay: 2.1 });
+        gsap.from(".first .big span", {
+          y: "100",
+          opacity: 0,
+          delay: 2.1,
+          stagger: 0.2,
+        });
+      } else if (active === 1) {
+        gsap.from(" .red", { y: "-1000", duration: 2 });
+        gsap.from(" .black", { y: "1000", duration: 2 });
+        gsap.from(" img", { x: "1000", duration: 2 });
+        gsap.from(" .small", { y: "100", opacity: 0, delay: 2.1 });
+        gsap.from(" .big span", {
+          y: "100",
+          opacity: 0,
+          delay: 2.1,
+          stagger: 0.2,
+        });
+      } else if (active === 2) {
+        gsap.from(".first .blue", { y: "-1000", duration: 2 });
+        gsap.from(".first .black", { y: "1000", duration: 2 });
+        gsap.from(".first img", { x: "1000", duration: 2 });
+        gsap.from(".first .small", { y: "100", opacity: 0, delay: 2.1 });
+        gsap.from(".first .big span", {
+          y: "100",
+          opacity: 0,
+          delay: 2.1,
+          stagger: 0.2,
+        });
+      } else if (active === 3) {
+        gsap.from(".first .blue", { y: "-1000", duration: 2 });
+        gsap.from(".first .black", { y: "1000", duration: 2 });
+        gsap.from(".first img", { x: "1000", duration: 2 });
+        gsap.from(".first .small", { y: "100", opacity: 0, delay: 2.1 });
+        gsap.from(".first .big span", {
+          y: "100",
+          opacity: 0,
+          delay: 2.1,
+          stagger: 0.2,
+        });
+      }
+      //
+    }, root);
+
+    console.log(active);
+
+    return () => ctx.revert();
+  }, [active]);
+
+  return (
+    <>
+      <div className="min-h-screen">
+        <Swiper
+          ref={root}
+          // slidesPerView={2}
+          pagination={{
+            el: ".swiper-pagination",
+            clickable: true,
+            renderBullet: function (index, className) {
+              return (
+                '<span class="' +
+                className +
+                '"> <span>' +
+                String(index + 1).padStart(2, "0") +
+                "</span></span>"
+              );
+            },
+          }}
+          onActiveIndexChange={(swiper) => {
+            console.log(swiper);
+            setActive(swiper.activeIndex);
+          }}
+          speed={1000}
+          direction="vertical"
+          // direction={getDirection()}
+
+          slidesPerView={1}
+          // spaceBetween={30}
+          mousewheel={true}
+          modules={[Pagination]}
+          className="mySwiper  h-screen "
+          // className="mySwiper h-screen"
+        >
+          <SwiperSlide
+            className={`w-full  h-full  ${
+              active === 0 || active === 0 ? "first" : "first"
+            }`}
+          >
+            <Project1 />
+          </SwiperSlide>
+          {/* item2 */}
+          <SwiperSlide
+            className={`w-full  h-full  ${active === 1 ? "first" : ""}`}
+          >
+            <div className="red"></div>
+            <div className="black"></div>
+            <div className="content relative">
+              <div className="title">
+                <div className="small">
+                  {/* Let's {isActive ? setActive(true) : setActive(false)} */}
+                  {/* {active && "active"} */}
+                </div>
+                <div className="big  md:text-[200px] lg:text-[280px] text-[150px] md:top-0 top-[50px]">
+                  <span>R</span>
+                  <span>A</span>
+                  <span>C</span>
+                  <span>E</span>
+                </div>
+              </div>
+              <div className="image relative">
+                {/* <Image alt="" src={img2} fill /> */}
+                {/* <img src="images/img-1.png" alt=""> */}
+              </div>
+              <div className="absolute bottom-10 p-2  gap-4 text-[#fffcef] justify-center text-xl flex items-center w-full">
+                <span className="font-bold z-20">
+                  <b>302</b> MPH
+                </span>
+                <span className="font-bold z-20">
+                  <b>0-100</b> KMPH
+                </span>
+                <span className="font-bold z-20">
+                  <b>350</b> KW
+                </span>
+                <button className=" py-2 px-3 border border-[#d23f3d] rounded-[10px] text-[#d23f3d] cursor-pointer hover:bg-[#d23f3d] hover:text-[#1a1a15]">
+                  Discover Now <i className="fas fa-arrow-right"></i>
+                </button>
+              </div>
+            </div>
+          </SwiperSlide>
+          {/* item3 */}
+          <SwiperSlide
+            className={`w-full h-full  ${active === 2 ? "first" : ""}`}
+          >
+            <div className="blue"></div>
+            <div className="black"></div>
+            <div className="content">
+              <div className="title">
+                <div className="small">
+                  {/* Let's {isActive ? setActive(true) : setActive(false)} */}
+                  {/* {active && "active"} */}
+                </div>
+                <div className="big md:text-[200px] lg:text-[280px] text-[150px] md:top-0 top-[50px]">
+                  <span>B</span>
+                  <span>l</span>
+                  <span>o</span>
+                  <span>g</span>
+                  <span>A</span>
+                  <span>p</span>
+                  <span>p</span>
+                </div>
+              </div>
+              <div className="image relative">
+                {/* <Image alt="" src={img3} fill /> */}
+              </div>
+              <div className="absolute bottom-10 p-2  gap-4 text-[#fffcef] justify-center text-xl flex items-center w-full">
+                <span className="font-bold z-20">
+                  <b>302</b> MPH
+                </span>
+                <span className="font-bold z-20">
+                  <b>0-100</b> KMPH
+                </span>
+                <span className="font-bold z-20">
+                  <b>350</b> KW
+                </span>
+                <button className=" py-2 px-3 border border-[#00d2fa] rounded-[10px] text-[#00d2fa] cursor-pointer hover:bg-[#00d2fa] hover:text-[#1a1a15]">
+                  Discover Now <i className="fas fa-arrow-right"></i>
+                </button>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide
+            className={`w-full h-full  ${active === 3 ? "first" : ""}`}
+          >
+            <div className="blue"></div>
+            <div className="black"></div>
+            <div className="content">
+              <div className="title">
+                <div className="small">
+                  {/* Let's {isActive ? setActive(true) : setActive(false)} */}
+                  {/* {active && "active"} */}
+                </div>
+                <div className="big md:text-[200px] lg:text-[280px] text-[150px] md:top-0 top-[50px]">
+                  <span>B</span>
+                  <span>r</span>
+                  <span>o</span>
+                  <span>g</span>
+                  <span>A</span>
+                  <span>p</span>
+                  <span>p</span>
+                </div>
+              </div>
+              <div className="image relative">
+                {/* <Image alt="" src={img3} fill /> */}
+              </div>
+              <div className="absolute bottom-10 p-2  gap-4 text-[#fffcef] justify-center text-xl flex items-center w-full">
+                <span className="font-bold z-20">
+                  <b>302</b> MPH
+                </span>
+                <span className="font-bold z-20">
+                  <b>0-100</b> KMPH
+                </span>
+                <span className="font-bold z-20">
+                  <b>350</b> KW
+                </span>
+                <button className=" py-2 px-3 border border-[#00d2fa] rounded-[10px] text-[#00d2fa] cursor-pointer hover:bg-[#00d2fa] hover:text-[#1a1a15]">
+                  Discover Now <i className="fas fa-arrow-right"></i>
+                </button>
+              </div>
+            </div>
+          </SwiperSlide>
+
+          <div className="swiper-pagination"></div>
+        </Swiper>
+      </div>
+    </>
+  );
+};
+
+export default Main;
--- src/app/page.js
+++ src/app/page.js
@@ -1,249 +1,99 @@
 "use client";
-import React, { useEffect, useRef, useState, useLayoutEffect } from "react";
+import React, { useRef, useLayoutEffect } from "react";
 import Head from "next/head";
-import { Swiper, SwiperSlide } from "swiper/react";
 <Head>
   <link rel="stylesheet" href="https://unpkg.com/swiper/css/swiper.min.css" />
   <link rel="stylesheet" href="https://unpkg.com/swiper/css/pagination.css" />
 </Head>;
-import { useSwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/pagination";
-import img1 from "@/../public/img-1.png";
-import img2 from "@/../public/img-2.png";
-import img3 from "@/../public/img-3.png";
-import { Pagination } from "swiper/modules";
 import { gsap } from "gsap";
-import Image from "next/image";
-import Project1 from "./components/project1/Project1";
+import Link from "next/link";
 
 export default function Home() {
-  const [active, setActive] = useState();
+  const comp = useRef(null);
 
-  const swiperSlide = useSwiperSlide();
-  const root = useRef();
-  useEffect(() => {
-    console.log(active, "active");
-  }, [active]);
   useLayoutEffect(() => {
     let ctx = gsap.context(() => {
-      if (active === 0 || active === undefined) {
-        gsap.from(".first .yellowproject1", { y: "-1000", duration: 2 });
-        gsap.from(".first .blackproject1", { y: "1000", duration: 2 });
-        gsap.from(".first .greenproject1", { y: "500", duration: 3 });
-        gsap.from(".first img", { x: "1000", duration: 2 });
-        gsap.from(".first .small", { y: "100", opacity: 0, delay: 2.1 });
-        gsap.from(".first .big span", {
-          y: "100",
+      const t1 = gsap.timeline();
+      t1.from("#intro-slider", {
+        xPercent: "-100",
+        duration: 1.3,
+        delay: 1.3,
+      })
+        .from(["#title-1", "#title-2", "#title-3"], {
           opacity: 0,
-          delay: 2.1,
-          stagger: 0.2,
-        });
-      } else if (active === 1) {
-        gsap.from(" .red", { y: "-1000", duration: 2 });
-        gsap.from(" .black", { y: "1000", duration: 2 });
-        gsap.from(" img", { x: "1000", duration: 2 });
-        gsap.from(" .small", { y: "100", opacity: 0, delay: 2.1 });
-        gsap.from(" .big span", {
-          y: "100",
+          y: "+=30",
+          stagger: 0.5,
+        })
+        .to(["#title-1", "#title-2", "#title-3"], {
           opacity: 0,
-          delay: 2.1,
-          stagger: 0.2,
-        });
-      } else if (active === 2) {
-        gsap.from(".first .blue", { y: "-1000", duration: 2 });
-        gsap.from(".first .black", { y: "1000", duration: 2 });
-        gsap.from(".first img", { x: "1000", duration: 2 });
-        gsap.from(".first .small", { y: "100", opacity: 0, delay: 2.1 });
-        gsap.from(".first .big span", {
-          y: "100",
+          y: "-=30",
+          delay: 1.3,
+          stagger: 0.5,
+        })
+        .to("#intro-slider", {
+          xPercent: "-100",
+          duration: 1.3,
+        })
+        .from("#welcome", {
           opacity: 0,
-          delay: 2.1,
-          stagger: 0.2,
+          duration: 0.5,
         });
-      } else if (active === 3) {
-        gsap.from(".first .blue", { y: "-1000", duration: 2 });
-        gsap.from(".first .black", { y: "1000", duration: 2 });
-        gsap.from(".first img", { x: "1000", duration: 2 });
-        gsap.from(".first .small", { y: "100", opacity: 0, delay: 2.1 });
-        gsap.from(".first .big span", {
-          y: "100",
-          opacity: 0,
-          delay: 2.1,
-          stagger: 0.2,
-        });
-      }
-      //
-    }, root);
-
-    console.log(active);
+    }, comp);
 
     return () => ctx.revert();
-  }, [active]);
+  }, []);
 
   return (
-    <div className="min-h-screen">
-      <Swiper
-        ref={root}
-        // slidesPerView={2}
-        pagination={{
-          el: ".swiper-pagination",
-          clickable: true,
-          renderBullet: function (index, className) {
-            return (
-              '<span class="' +
-              className +
-              '"> <span>' +
-              String(index + 1).padStart(2, "0") +
-              "</span></span>"
-            );
-          },
-        }}
-        onActiveIndexChange={(swiper) => {
-          console.log(swiper);
-          setActive(swiper.activeIndex);
-        }}
-        speed={1000}
-        direction="vertical"
-        // direction={getDirection()}
-
-        slidesPerView={1}
-        // spaceBetween={30}
-        mousewheel={true}
-        modules={[Pagination]}
-        className="mySwiper  h-screen "
-        // className="mySwiper h-screen"
+    <div className="relative" ref={comp}>
+      <div
+        id="intro-slider"
+        className="h-screen p-10 bg-white text-black text-center absolute top-0 left-0 z-10 w-full flex flex-col gap-10 tracking-tight"
       >
-        <SwiperSlide
-          className={`w-full  h-full  ${
-            active === 0 || active === 0 ? "first" : "first"
-          }`}
-        >
-          <Project1 />
-        </SwiperSlide>
-        {/* item2 */}
-        <SwiperSlide
-          className={`w-full  h-full  ${active === 1 ? "first" : ""}`}
-        >
-          <div className="red"></div>
-          <div className="black"></div>
-          <div className="content relative">
-            <div className="title">
-              <div className="small">
-                {/* Let's {isActive ? setActive(true) : setActive(false)} */}
-                {/* {active && "active"} */}
-              </div>
-              <div className="big  md:text-[200px] lg:text-[280px] text-[150px] md:top-0 top-[50px]">
-                <span>R</span>
-                <span>A</span>
-                <span>C</span>
-                <span>E</span>
-              </div>
-            </div>
-            <div className="image relative">
-              {/* <Image alt="" src={img2} fill /> */}
-              {/* <img src="images/img-1.png" alt=""> */}
-            </div>
-            <div className="absolute bottom-10 p-2  gap-4 text-[#fffcef] justify-center text-xl flex items-center w-full">
-              <span className="font-bold z-20">
-                <b>302</b> MPH
-              </span>
-              <span className="font-bold z-20">
-                <b>0-100</b> KMPH
-              </span>
-              <span className="font-bold z-20">
-                <b>350</b> KW
-              </span>
-              <button className=" py-2 px-3 border border-[#d23f3d] rounded-[10px] text-[#d23f3d] cursor-pointer hover:bg-[#d23f3d] hover:text-[#1a1a15]">
-                Discover Now <i className="fas fa-arrow-right"></i>
-              </button>
-            </div>
-          </div>
-        </SwiperSlide>
-        {/* item3 */}
-        <SwiperSlide
-          className={`w-full h-full  ${active === 2 ? "first" : ""}`}
-        >
-          <div className="blue"></div>
-          <div className="black"></div>
-          <div className="content">
-            <div className="title">
-              <div className="small">
-                {/* Let's {isActive ? setActive(true) : setActive(false)} */}
-                {/* {active && "active"} */}
-              </div>
-              <div className="big md:text-[200px] lg:text-[280px] text-[150px] md:top-0 top-[50px]">
-                <span>B</span>
-                <span>l</span>
-                <span>o</span>
-                <span>g</span>
-                <span>A</span>
-                <span>p</span>
-                <span>p</span>
-              </div>
-            </div>
-            <div className="image relative">
-              {/* <Image alt="" src={img3} fill /> */}
-            </div>
-            <div className="absolute bottom-10 p-2  gap-4 text-[#fffcef] justify-center text-xl flex items-center w-full">
-              <span className="font-bold z-20">
-                <b>302</b> MPH
-              </span>
-              <span className="font-bold z-20">
-                <b>0-100</b> KMPH
-              </span>
-              <span className="font-bold z-20">
-                <b>350</b> KW
-              </span>
-              <button className=" py-2 px-3 border border-[#00d2fa] rounded-[10px] text-[#00d2fa] cursor-pointer hover:bg-[#00d2fa] hover:text-[#1a1a15]">
-                Discover Now <i className="fas fa-arrow-right"></i>
-              </button>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide
-          className={`w-full h-full  ${active === 3 ? "first" : ""}`}
-        >
-          <div className="blue"></div>
-          <div className="black"></div>
-          <div className="content">
-            <div className="title">
-              <div className="small">
-                {/* Let's {isActive ? setActive(true) : setActive(false)} */}
-                {/* {active && "active"} */}
-              </div>
-              <div className="big md:text-[200px] lg:text-[280px] text-[150px] md:top-0 top-[50px]">
-                <span>B</span>
-                <span>r</span>
-                <span>o</span>
-                <span>g</span>
-                <span>A</span>
-                <span>p</span>
-                <span>p</span>
-              </div>
-            </div>
-            <div className="image relative">
-              {/* <Image alt="" src={img3} fill /> */}
-            </div>
-            <div className="absolute bottom-10 p-2  gap-4 text-[#fffcef] justify-center text-xl flex items-center w-full">
-              <span className="font-bold z-20">
-                <b>302</b> MPH
-              </span>
-              <span className="font-bold z-20">
-                <b>0-100</b> KMPH
-              </span>
-              <span className="font-bold z-20">
-                <b>350</b> KW
-              </span>
-              <button className=" py-2 px-3 border border-[#00d2fa] rounded-[10px] text-[#00d2fa] cursor-pointer hover:bg-[#00d2fa] hover:text-[#1a1a15]">
-                Discover Now <i className="fas fa-arrow-right"></i>
-              </button>
-            </div>
-          </div>
-        </SwiperSlide>
+        <h1 className="text-9xl" id="title-1">
+          Gokul's
+        </h1>
+        <h1 className="text-9xl" id="title-2">
+          Project Stash
+        </h1>
+        <h1 className="text-9xl" id="title-3">
+          is here...
+        </h1>
+      </div>
 
-        <div className="swiper-pagination"></div>
-      </Swiper>
+      <div className="h-screen flex bg-gray-950 justify-center place-items-center">
+        <h1
+          id="welcome"
+          className="text-9xl mb-10 font-bold text-gray-100 font-spaceGrotesk"
+        >
+          Welcome.
+          <p className="text-2xl text-center font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+            NOTE - Set your browser zoom level to 90%
+          </p>
+          <Link href="/mainproject">
+            {" "}
+            <div
+              id="welcome"
+              className="flex mt-10 items-center justify-center bg-zinc-950"
+            >
+              <div
+                href="/"
+                className="bg-[#4637B4] group relative inline-flex items-center overflow-hidden rounded-full px-8 py-3 transition"
+              >
+                <div className="absolute inset-0 flex items-center [container-type:inline-size]">
+                  <div className="absolute h-[100cqw] w-[100cqw] animate-spin bg-[conic-gradient(from_0_at_50%_50%,rgba(255,255,255,0.5)_0deg,transparent_60deg,transparent_300deg,rgba(255,255,255,0.5)_360deg)] opacity-0 transition duration-300 group-hover:opacity-100"></div>
+                </div>
+                <div className="absolute inset-0.5 rounded-full bg-black/90"></div>
+                <div className="absolute bottom-0 left-1/2 h-1/3 w-4/5 -translate-x-1/2 rounded-full bg-white/10 opacity-50 blur-md transition-all duration-500 group-hover:h-2/3 group-hover:opacity-100"></div>
+                <div className="relative inline-flex items-center gap-2">
+                  <span className="font-mona mt-px bg-gradient-to-b from-white/25 to-white bg-clip-text text-lg font-medium text-transparent transition-all duration-200">
+                    Click here to begin
+                  </span>
+                </div>
+              </div>
+            </div>
+          </Link>
+        </h1>
+      </div>
     </div>
   );
 }
